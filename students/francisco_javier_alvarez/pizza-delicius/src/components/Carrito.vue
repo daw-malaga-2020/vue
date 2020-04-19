87ce49dce1211d2b229bd29fe4adca4055d1551b
Francisco J. Álvarez <fcojalvarezrodriguez@gmail.com>
2020-04-19 20:03:32 +0200

    pizza-delicius

---
+++ students/francisco_javier_alvarez/pizza-delicius/src/components/Carrito.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="ftco-intro">
+  <div class="container-wrap">
+    <div class="wrap d-md-flex">
+      <div class="info">
+        <div class="row no-gutters">
+          <div class="col-md-4 d-flex">
+            <div class="text">
+              <h3>YOU ORDER</h3>
+              <p>Add your delicious order and press "Order" button </p>
+            </div>
+          </div>
+          <div class="col-md-8 d-flex">
+            <div class="text">
+              <table class="table table-sm">
+                <thead>
+                  <tr>
+                    <th scope="col">Product</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Pizza Hawai</td>
+                    <td>1</td>
+                    <td>10,5 €</td>
+                  </tr>
+
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="social d-md-flex pl-md-5 p-4 align-items-center">
+        <div class="text">
+          <h2>TOTAL</h2>
+          <h4>10,5 €</h4>
+        </div>
+        <a href="#" class="p-3 px-xl-4 py-xl-3 btn-white btn btn-outline-white">Order Now</a>
+                                                                                                                            </div>
+                                                                                                                    </div>
+                                                                                                            </div>
+
+    </section>
+</template>
---
+++ students/francisco_javier_alvarez/pizza-delicius/src/pages/Contacto.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <MainNav></MainNav>
+        <SliderContact></SliderContact>
+        <FormContact></FormContact>
+        <MainFooter></MainFooter>
+    </div>
+</template>
+
+<script>
+
+import MainNav from '../components/MainNav'
+import SliderContact from '../components/SliderContact'
+import FormContact from '../components/FormContact'
+import MainFooter from '../components/MainFooter'
+
+export default {
+    name: 'Contacto',
+    components: {
+        MainNav,
+        SliderContact,
+        FormContact,
+        MainFooter
+    }
+}
+</script>
