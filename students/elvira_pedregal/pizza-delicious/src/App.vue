411b6feadd3410382be0581fe00fecf0a4d28d1f
elvira <elvirapedregal@hotmail.es>
2020-04-20 10:32:01 +0200

    first-commmit

---
+++ students/elvira_pedregal/pizza-delicious/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!--<Home></Home>-->
+    <Pedidos></Pedidos>
+  </div>
+</template>
+
+<script>
+import Home from './pages/Home'
+import Pedidos from './pages/Pedidos'
+import Contacto from './pages/Contacto'
+import Blog from './pages/Blog'
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+    Pedidos
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
