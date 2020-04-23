921ead70d36f1347af1c10b2ddf1d9328c9104af
Francisco J. Álvarez <fcojalvarezrodriguez@gmail.com>
2020-04-23 17:15:36 +0200

    Update with vue-router

--- students/francisco_javier_alvarez/pizza-delicius/src/pages/Blog.vue
+++ students/francisco_javier_alvarez/pizza-delicius/src/pages/Blog.vue
@@ -1,26 +1,20 @@
 <template>
     <div>
-        <MainNav></MainNav>
         <SliderBlog></SliderBlog>
         <EntradasBlog></EntradasBlog>
-        <MainFooter></MainFooter>
     </div>
 </template>
 
 <script>
 
-import MainNav from '../components/MainNav'
 import SliderBlog from '../components/SliderBlog'
 import EntradasBlog from '../components/EntradasBlog'
-import MainFooter from '../components/MainFooter'
 
 export default {
     name: 'Blog',
     components: {
-        MainNav,
         SliderBlog,
         EntradasBlog,
-        MainFooter
     }
 }
 </script>
--- students/francisco_javier_alvarez/pizza-delicius/src/pages/Pedidos.vue
+++ students/francisco_javier_alvarez/pizza-delicius/src/pages/Pedidos.vue
@@ -1,29 +1,23 @@
 <template>
     <div>
-        <MainNav></MainNav>
         <SliderPedido></SliderPedido>
         <MenuPedidos></MenuPedidos>
         <Carrito></Carrito>
-        <MainFooter></MainFooter>
     </div>
 </template>
 
 <script>
 
-import MainNav from '../components/MainNav'
 import SliderPedido from '../components/SliderPedido'
 import MenuPedidos from '../components/MenuPedidos'
 import Carrito from '../components/Carrito'
-import MainFooter from '../components/MainFooter'
 
 export default {
     name: 'Pedidos',
     components: {
-        MainNav,
         SliderPedido,
         MenuPedidos,
         Carrito,
-        MainFooter
     }
 }
 </script>
