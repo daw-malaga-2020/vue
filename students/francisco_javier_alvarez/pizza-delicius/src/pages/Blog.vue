94a4bcce06986b4f785027233f41b96da4e069d2
Francisco J. Álvarez <fcojalvarezrodriguez@gmail.com>
2020-04-29 14:47:55 +0200

    Subida ejercicios

--- students/francisco_javier_alvarez/pizza-delicius/src/pages/Blog.vue
+++ students/francisco_javier_alvarez/pizza-delicius/src/pages/Blog.vue
@@ -1,19 +1,16 @@
 <template>
     <div>
-        <SliderBlog></SliderBlog>
         <EntradasBlog></EntradasBlog>
     </div>
 </template>
 
 <script>
 
-import SliderBlog from '../components/SliderBlog'
 import EntradasBlog from '../components/EntradasBlog'
 
 export default {
     name: 'Blog',
     components: {
-        SliderBlog,
         EntradasBlog,
     }
 }
