808d3038a5d8a9d4b884ca619cf9044b976b64ae
Francisco J. Álvarez <you@example.com>
2020-04-30 09:31:55 +0200

    MaldonadoWeather with Vue

---
+++ students/francisco_javier_alvarez/pizza-delicius/src/components/Slider.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="home-slider owl-carousel img">
+        <div class="slider-item" style="background-image: url(images/bg_3.jpg);">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
+                    <div class="col-md-7 col-sm-12 text-center ">
+                        <span class="subheading">{{ HeadingWelcome }}</span>
+                        <h1 class="mb-4">{{ titleSlider }}</h1>
+                        <p class="mb-4 mb-md-5">
+                            {{ descriptionSlider }}
+                        </p>
+                        <p>
+                            <slot name="btnOrder"></slot>
+				        </p>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </section>
+</template>
+
+<script>
+export default {
+    props: ['HeadingWelcome', 'titleSlider', 'descriptionSlider', 'btnOrder']
+}
+</script>
---
+++ students/francisco_javier_alvarez/pizza-delicius/src/partials/Pizzas.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div class="row no-gutters d-flex" v-for="(pizza, i) in pizzas" :key="i">
+        <div class="col-lg-4 d-flex d-block">
+            <div class="services-wrap d-flex" >
+                <a href="#" class="img" :style="pizza.image"></a>
+                <div class="text p-4" >
+                    <h3>{{ pizza.title }}</h3>
+                    <p>
+                        {{ pizza.description }}
+                    </p>
+                    <p class="price">
+                        <span>${{pizza.price.toFixed(2)}}</span>
+                        <a href="#" class="ml-2 btn btn-white btn-outline-white">Order</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            pizzas: [
+                {image: 'background-image: url(images/pizza-1.jpg);', title: 'Italian Pizza', description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia', price: 2.90},
+                {image: 'background-image: url(images/pizza-2.jpg);', title: 'Greek Pizza', description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia', price: 2.90},
+                {image: 'background-image: url(images/pizza-3.jpg);', title: 'Caucasian Pizza', description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia', price: 2.90},
+                {image: 'background-image: url(images/pizza-4.jpg);', title: 'American Pizza', description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia', price: 2.90},
+                {image: 'background-image: url(images/pizza-5.jpg);', title: 'Tomatoe Pie', description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia', price: 2.90},
+                {image: 'background-image: url(images/pizza-6.jpg);', title: 'Margherita', description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia', price: 2.90}
+            ]
+        }
+    }
+}
+</script>
