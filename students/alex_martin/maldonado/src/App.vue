37f61deb79318f9165b69e9f0623b3b79ec2f29a
Álex Martín <alex@alexdw.com>
2020-04-30 09:40:26 +0200

    Minor issues

--- students/alex_martin/maldonado/src/App.vue
+++ students/alex_martin/maldonado/src/App.vue
@@ -8,7 +8,7 @@
                                 <select id="locationSelect" class="form-control" v-model="countrySelected" @change="loadWeatherSelected">
                                     <option v-for="item in locations" :value="item.value" :key="item.id" >{{ item.name }}</option>
                                 </select>
-                                <h1 class="heading">{{weather}}</h1>
+                                <h1 class="heading">{{weather}} {{name}}</h1>
 
                                 <p class="temp">
                                     <span class="temp-value">{{ temp }}</span>
@@ -54,11 +54,13 @@ export default {
           {id: 1, name: "Málaga", value: "Málaga, es" }, 
           {id: 2, name:"Sevilla", value:"Sevilla, es"}, 
           {id: 3, name:"Huelva", value: "Huelva, es"},
+          {id: 4, name:"Pyongyang", value: "Pyongyang, North Korea"},
       ],
       temp: "",
       hum: "",
       weather: "",
       wind:"",
+      name: ""
     }
   },
   methods: {
@@ -68,11 +70,17 @@ export default {
       const unit = "metric";
       
       let URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.countrySelected}&appid=${token}&lang=${lang}&units=${unit}`;  
-      let response= await this.axios.get(URI)
-      this.weather=response.data.weather[0].description
-      this.temp=Math.trunc(response.data.main.temp)
-      this.wind = response.data.wind.speed
-      this.hum= response.data.main.humidity 
+      
+      try{
+        let response= await this.axios.get(URI)
+        this.weather=response.data.weather[0].description
+        this.temp=Math.trunc(response.data.main.temp)
+        this.wind = response.data.wind.speed
+        this.hum= response.data.main.humidity 
+        this.name = response.data.name
+      }catch(e){
+          alert(e.message)
+      }
     }
   },
   mounted(){
