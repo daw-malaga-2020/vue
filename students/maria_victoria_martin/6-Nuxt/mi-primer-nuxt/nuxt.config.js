a8e58f7a8efecf89bace36ca20736b0e532bd9d4
vicky <m.victoria_14@hotmail.com>
2020-05-17 12:57:31 +0200

    nuxt

---
+++ students/maria_victoria_martin/6-Nuxt/mi-primer-nuxt/nuxt.config.js
@@ -0,0 +1,81 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/vue-notifications',
+    { src: '@/plugins/vue-youtube.js', ssr: false },
+
+    ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+    "@nuxtjs/sitemap",
+    ["@nuxtjs/firebase",
+   {
+    config: {
+      apiKey: "AIzaSyB7B_plmn-CjHz6C6ueVe6meY03b7UNbeA",
+      authDomain: "pizza-delicious-96c19.firebaseapp.com",
+      databaseURL: "https://pizza-delicious-96c19.firebaseio.com",
+      projectId: "pizza-delicious-96c19",
+      storageBucket: "pizza-delicious-96c19.appspot.com",
+      messagingSenderId: "937513632491",
+      appId: "1:937513632491:web:3e55617d1f2c3279c134ae",
+      measurementId: "G-P8KLH0KVEE"
+    },
+    services: {
+     // auth: true // Just as example. Can be any other service.
+     firestore: true,
+     storage: true,
+    }
+  }]
+
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
